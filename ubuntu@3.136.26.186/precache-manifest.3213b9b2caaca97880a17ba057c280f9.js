9088ad5390c30b0db3f958dfde50bab50d51ea89
yagya <prakashyagya@gmail.com>
2020-09-23 21:41:01 +0530

    added changes in user

---
+++ ubuntu@3.136.26.186/precache-manifest.3213b9b2caaca97880a17ba057c280f9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ab33f836c5ca5c82ce93278d802936",
+    "url": "/index.html"
+  },
+  {
+    "revision": "184bd0c6510d40125336",
+    "url": "/static/css/2.04cb8a36.chunk.css"
+  },
+  {
+    "revision": "5577d30ed5a5b0e9c6ba",
+    "url": "/static/css/main.f58c6434.chunk.css"
+  },
+  {
+    "revision": "184bd0c6510d40125336",
+    "url": "/static/js/2.e29383eb.chunk.js"
+  },
+  {
+    "revision": "5577d30ed5a5b0e9c6ba",
+    "url": "/static/js/main.290e892b.chunk.js"
+  },
+  {
+    "revision": "302a62736d414a603fc3",
+    "url": "/static/js/runtime~main.0ef13994.js"
+  },
+  {
+    "revision": "e75e0e6b4671e8fe8b63870a15150a5f",
+    "url": "/static/media/imagee.e75e0e6b.png"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "97ff0bfdcc1c29e79af56984d63fb934",
+    "url": "/static/media/reactlogo.97ff0bfd.png"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "0e1a42854a065b411b531a38c0f2d287",
+    "url": "/static/media/warning.0e1a4285.png"
+  }
+]);
